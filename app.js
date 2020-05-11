34938f617fe097bc2ccbdcb15a2bd9ad93b266b8
Bijaya Limbu <bl18aat@herts.ac.uk>
2020-05-11 11:46:00 +0100

    initialise readline

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+
+var readlineSync = require('readline-sync');
 console.log("Welcome to this console game");
+
+var userName = readlineSync.question('May I have your name?');
+console.log("Welcome," + userName);
